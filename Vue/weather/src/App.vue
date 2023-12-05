8d5c7db8208bfd495515ca90c26432b924b31f7e
cheng-bingwu <1474302241@qq.com>
2023-12-05 21:50:26 +0800

    weather

---
+++ Vue/weather/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- 所有配置了路由的.vue文件都展示在这个口子上 -->
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {}
+  }
+}
+</script>
+
+<style lang="css">
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
