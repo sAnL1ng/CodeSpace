c1a8a2d04896f9a5974e29a9535659e8d4ea3b41
sAnL1ng <1474302241@qq.com>
2023-12-21 21:27:00 +0800

    js进阶之深入理解DP&贪心 和 闭包收集参数

---
+++ js/curry/1.js
@@ -0,0 +1,28 @@
+function add(a,b,c){
+    return a + b + c
+}
+
+//函数的颗粒化
+function curry(fn){
+    // 收集参数 用闭包 用数组arr存放参数
+    // 到位了才运行 arr.length === fn.length fn(...arr)
+    // 返回收集参数的函数
+    // rest 运算符 args是数组 这个数组在闭包空间 闭包空间里的变量专业名词为自由变量
+    // args
+    let judge = (...args) => {
+        // let a = 1
+        if(args.length === fn.length) return fn(...args)
+        // 返回函数，继续收集参数
+        return (...arg) => judge(...args,...arg) 
+    }
+    return judge
+}
+
+// 有时候不会一次性给到
+//  js curry 颗粒化
+// 每次接受一个参数
+
+console.log(add.length);
+// add(1)(2)(3)
+const curryAdd = curry(add)
+curryAdd(1)(2)(3)
