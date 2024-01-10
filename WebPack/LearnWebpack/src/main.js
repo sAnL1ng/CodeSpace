fbc9720888160624a7fedcc118aedbb9fc7b36af
sAnL1ng <1474302241@qq.com>
2024-01-10 21:25:50 +0800

    学习项目如何打包上线

---
+++ WebPack/LearnWebpack/src/main.js
@@ -0,0 +1,9 @@
+// import { add } from './tools/add.js'
+// import './style/index.css'
+// console.log(add(1,2));
+// console.log('hello');
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
