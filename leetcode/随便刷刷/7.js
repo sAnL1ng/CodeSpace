44d4b5d39c686be8731212c0b096daa6d7e4204b
SANL1NG <1474302241@qq.com>
2024-02-24 22:02:24 +0800

    leetcode

---
+++ leetcode/随便刷刷/7.js
@@ -0,0 +1,13 @@
+var x = 123
+
+var reverse = function(x) {
+    let res = 0;
+    while(x){
+        res = res * 10 + x % 10;
+        if(res > Math.pow(2, 31) - 1 || res < Math.pow(-2, 31)) return 0;
+        x = ~~(x / 10);
+    }
+    return res;
+};
+
+console.log(reverse(x));
