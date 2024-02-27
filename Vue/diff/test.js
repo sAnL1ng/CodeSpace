e4037ad1ebc4813c7c5f05e02aa2e82aeec943ff
SANL1NG <1474302241@qq.com>
2024-02-27 23:10:45 +0800

    diff算法

---
+++ Vue/diff/test.js
@@ -0,0 +1,17 @@
+let newDom = [
+    {
+        tagName:'li',
+        key: 0,
+        value: 'vue'
+    },
+    {
+        tagName:'li',
+        key: 1,
+        value: 'css'
+    },
+    {
+        tagName:'li',
+        key: 2,
+        value: 'html'
+    },
+]
